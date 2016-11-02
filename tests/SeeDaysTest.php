3249f19a2a34481b5c02b66ae801ce62e6b83cce
Richard Styles <richard.styles@brunningandprice.co.uk>
2016-11-02 19:21:26 +0000

    Update initial test for phpunit

--- tests/SeeDaysTest.php
+++ tests/SeeDaysTest.php
@@ -4,16 +4,16 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class SeeDaysTest extends TestCase
 {
     /**
      * A basic functional test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testDaysIsShownOnRootPage()
     {
         $this->visit('/')
-             ->see('Laravel');
+             ->see('Days');
     }
 }
